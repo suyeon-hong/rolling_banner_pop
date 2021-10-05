5eab4a65853b0b3e893790c68714245fb7311587
suyeon-hong <bellahong95@gmail.com>
2021-10-05 17:28:06 +0900

    aside 페이지 추가

--- js/custom.js
+++ js/custom.js
@@ -11,6 +11,23 @@ $(".listBox").on("mouseleave", function(){
     timer = setInterval(move, 20);
 });
 
+$(".list li").on("click", function(e){
+    e.preventDefault();
+
+    $("body").append(
+        $("<aside class='pop'>").append(
+            $("<div class='con'>"),
+            $("<span class='btnClose'>").text("close")
+        ).fadeIn(500)
+    )
+});
+
+$(".pop .btnClose").on("click", function(e){
+    e.preventDefault();
+
+    $(".pop").fadeOut();
+});
+
 function move(){
     if (num <= -240){
         num = 0;
